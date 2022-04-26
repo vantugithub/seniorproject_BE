4a2bc68c69e2bfd08e285fec4c5a04098e8547b9
unknown <nguyenvantu11041999@gmail.com>
2022-04-26 16:28:48 +0700

    nothing

--- src/main/java/project/instagram/controller/AccountController.java
+++ src/main/java/project/instagram/controller/AccountController.java
@@ -6,7 +6,6 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
