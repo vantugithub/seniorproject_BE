38d8889e06877b215bf67919e40f2ebfe89c4901
unknown <nguyenvantu11041999@gmail.com>
2022-04-26 15:07:44 +0700

    fix origin

--- src/main/java/project/instagram/security/WebMvcConfig.java
+++ src/main/java/project/instagram/security/WebMvcConfig.java
@@ -9,13 +9,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @SuppressWarnings("deprecation")
 @Configuration
 public class WebMvcConfig {
-	@SuppressWarnings("deprecation")
 	@Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+                registry.addMapping("/**").allowedOrigins("*");
             }
         };
     }
