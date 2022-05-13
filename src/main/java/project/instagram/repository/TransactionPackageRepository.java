7f5ca30a5714882d60986b3564b6a088b7d10bf0
unknown <nguyenvantu11041999@gmail.com>
2022-05-13 17:47:34 +0700

    fix TransactionPackageRepository

--- src/main/java/project/instagram/repository/TransactionPackageRepository.java
+++ src/main/java/project/instagram/repository/TransactionPackageRepository.java
@@ -2,6 +2,7 @@ package project.instagram.repository;
 
 import java.util.Date;
 import java.util.Optional;
+import java.util.Set;
 
 import javax.transaction.Transactional;
 
@@ -9,6 +10,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import project.instagram.entity.Client;
 import project.instagram.entity.TransactionPackage;
 
 @Repository
@@ -23,5 +25,21 @@ public interface TransactionPackageRepository extends JpaRepository<TransactionP
 			+ "ORDER BY t.id DESC LIMIT 1", nativeQuery = true)
 	public Optional<TransactionPackage> findByExpiredDateGreaterThanEqualAndClientAndChildPackage(
 			String packageType, String clientId, Date currentDate);
+	
+	@Query(value = "SELECT "
+			+ "t.id, "
+			+ "t.client, "
+			+ "t.expired_date, "
+			+ "t.issued_date, "
+			+ "t.package "
+			+ "FROM "
+			+ "transaction_packages t "
+			+ "WHERE "
+			+ "t.client= ?1 "
+			+ "AND (t.expired_date>= ?2 "
+			+ "OR t.expired_date is null) "
+			+ "ORDER BY "
+			+ "t.expired_date ASC", nativeQuery = true)
+	public Set<TransactionPackage> findAllValidTransactionPackages(Client client,Date currentDate);
 	
 }
