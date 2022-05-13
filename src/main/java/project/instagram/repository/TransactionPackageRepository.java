5594192fd89df20187bcf337eb51c7f2268c5ad9
unknown <nguyenvantu11041999@gmail.com>
2022-05-13 17:21:49 +0700

    fix conflit

--- src/main/java/project/instagram/repository/TransactionPackageRepository.java
+++ src/main/java/project/instagram/repository/TransactionPackageRepository.java
@@ -17,6 +17,15 @@ import project.instagram.entity.TransactionPackage;
 @Transactional
 public interface TransactionPackageRepository extends JpaRepository<TransactionPackage, Integer> {
 	
+	@Query(value = "SELECT t.id, t.expired_date, t.issued_date, t.package, t.client "
+			+ "FROM transaction_packages t WHERE t.package IN "
+			+ "(SELECT p.id FROM packages p WHERE p.type_of_package IN "
+			+ "(SELECT tp.id FROM type_of_package tp WHERE tp.name = ?1 AND t.client = ?2 )) "
+			+ "AND t.expired_date > ?3 "
+			+ "ORDER BY t.id DESC LIMIT 1", nativeQuery = true)
+	public Optional<TransactionPackage> findByExpiredDateGreaterThanEqualAndClientAndChildPackage(
+			String packageType, String clientId, Date currentDate);
+	
 	@Query(value = "SELECT "
 			+ "t.id, "
 			+ "t.client, "
@@ -32,13 +41,4 @@ public interface TransactionPackageRepository extends JpaRepository<TransactionP
 			+ "ORDER BY "
 			+ "t.expired_date ASC", nativeQuery = true)
 	public Set<TransactionPackage> findAllValidTransactionPackages(Client client,Date currentDate);
-	
-	@Query(value = "SELECT t.id, t.expired_date, t.issued_date, t.package, t.client "
-			+ "FROM transaction_packages t WHERE t.package IN "
-			+ "(SELECT p.id FROM packages p WHERE p.type_of_package IN "
-			+ "(SELECT tp.id FROM type_of_package tp WHERE tp.name = ?1 AND t.client = ?2 )) "
-			+ "AND t.expired_date > ?3 "
-			+ "ORDER BY t.id DESC LIMIT 1", nativeQuery = true)
-	public Optional<TransactionPackage> findByExpiredDateGreaterThanEqualAndClientAndChildPackage(
-			String packageType, String clientId, Date currentDate);
 }
