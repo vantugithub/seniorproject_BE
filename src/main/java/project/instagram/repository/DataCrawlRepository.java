5e022fe95b232f269bca80d382a5d00c0d8474b4
unknown <nguyenvantu11041999@gmail.com>
2022-05-31 13:56:47 +0700

    Staff controller

---
+++ src/main/java/project/instagram/repository/DataCrawlRepository.java
@@ -0,0 +1,24 @@
+package project.instagram.repository;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import project.instagram.entity.DataCrawl;
+import project.instagram.entity.Hashtag;
+
+@Repository
+public interface DataCrawlRepository extends JpaRepository<DataCrawl, String> {
+	List<DataCrawl> findAllByCreatedDatePostLessThanEqualAndHashtagOrderByCreatedDatePostDesc(Date date,
+			Hashtag hashtag);
+
+	List<DataCrawl> findAllByCreatedDatePostGreaterThanEqualAndHashtagOrderByCreatedDatePostDesc(Date date,
+			Hashtag hashtag);
+
+	Page<DataCrawl> findAllByCreatedDatePostLessThanEqualAndHashtagOrderByCreatedDatePostDesc(Pageable pageable,
+			Date date, Hashtag hashtag);
+}
